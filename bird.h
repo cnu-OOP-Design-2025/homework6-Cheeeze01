19c3cb66a26f83b36bc0a8ccb75bf95d8afc2391
Cheeeze01 <alex2878001@gmail.com>
2025-10-28 14:49:28 +0900

    homework

--- bird.h
+++ bird.h
@@ -2,29 +2,62 @@
 
 #include <iostream>
 
-class Bird {
-    std::string species; 
+class Bird
+{
+    std::string species;
+
 public:
-    Bird(){}
-    Bird(std::string species): species(species){
+    Bird() {}
+    Bird(std::string species) : species(species)
+    {
         std::cout << species << " has borned" << std::endl;
     }
 
-    virtual void display() {
+    virtual void display()
+    {
         std::cout << "Species: " << species << std::endl;
     }
 
-    virtual void fly() {
+    virtual void fly()
+    {
         std::cout << "This bird can fly." << std::endl;
     }
 
-    virtual void swim() {
+    virtual void swim()
+    {
         std::cout << "This bird can swim." << std::endl;
     }
 
     virtual void cry() = 0;
 
-    virtual ~Bird() {
+    virtual ~Bird()
+    {
         std::cout << species << " has died" << std::endl;
-     }
+    }
+};
+
+class Duck : public Bird
+{
+public:
+    Duck();
+    ~Duck() override;
+    void cry() override;
+};
+
+class Penguin : public Bird
+{
+public:
+    Penguin();
+    ~Penguin() override;
+    void cry() override;
+    void fly() override;
+};
+
+class Eagle : public Bird
+{
+public:
+    Eagle();
+    ~Eagle() override;
+    void cry() override;
+    void swim() override;
 };
